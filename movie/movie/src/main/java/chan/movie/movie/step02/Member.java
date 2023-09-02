c4041627c780ac17edf6133782766d53d2c88b87
chan <sniwoo98@naver.com>
2023-09-03 00:40:03 +0900

    feat : totalMember 메서드 생성
    23.09.03

--- movie/movie/src/main/java/chan/movie/movie/step02/Member.java
+++ movie/movie/src/main/java/chan/movie/movie/step02/Member.java
@@ -1,9 +1,20 @@
 package chan.movie.movie.step02;
 
+import chan.movie.money.Money;
+
 public class Member {
     private int adult; // 그대로
     private int teen; // 2000원 할인
     private int child; // 5000원 할인
+    private int fee;
 
+    public Member(int adult, int teen, int child) {
+        this.adult = adult;
+        this.teen = teen;
+        this.child = child;
+    }
 
+    public int totalMember() {
+        return adult + teen + child;
+    }
 }
