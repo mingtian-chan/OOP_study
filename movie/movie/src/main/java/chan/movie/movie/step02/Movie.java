b176fbf129728eac66907a9b9e4cd03b91b058f4
chan <sniwoo98@naver.com>
2023-09-03 00:40:35 +0900

    feat : MovieCategory를 생성해서 그 안에 영화 가격 정보를 입력
    23.09.03

--- movie/movie/src/main/java/chan/movie/movie/step02/Movie.java
+++ movie/movie/src/main/java/chan/movie/movie/step02/Movie.java
@@ -10,19 +10,22 @@ public class Movie {
     private Duration runningTime;
     private Money fee;
     private DiscountPolicy discountPolicy;
+    private MovieCategory movieCategory;
 
-    public Movie(String title, Duration runningTime, Money fee, DiscountPolicy discountPolicy) {
+    public Movie(String title, Duration runningTime, Money fee, DiscountPolicy discountPolicy,MovieCategory movieCategory) {
         this.title = title;
         this.runningTime = runningTime;
         this.fee = fee;
         this.discountPolicy = discountPolicy;
+        this.movieCategory = movieCategory;
     }
-
+    // getFee 는 Movie 보다 Screening에 있는게 더 합리적인거같아.
+//     왜냐면 Screening 마다 금액이 다 다르니까 굳이 Movie에서 받을 이유가 없어보임.
     public Money getFee() {
         return fee;
     }
 
-    public Money calculateMovieFee(Screening screening) {
+    public Money calculateTotalMovieFee(Screening screening) {
         return fee.minus(discountPolicy.calculateDiscountAmount(screening));
     }
 }
