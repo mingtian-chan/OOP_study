a233793143bde61b493b3742c430db89d55ac9cf
chan <sniwoo98@naver.com>
2023-09-03 01:59:03 +0900

    feat : Member 내에 1인당 할인액 을 넣음
    23.09.03

--- movie/movie/src/main/java/chan/movie/movie/step02/Member.java
+++ movie/movie/src/main/java/chan/movie/movie/step02/Member.java
@@ -6,6 +6,10 @@ public class Member {
     private int adult; // 그대로
     private int teen; // 2000원 할인
     private int child; // 5000원 할인
+
+    private int adultDiscount = 0;
+    private int teenDiscount = 2000;
+    private int childDiscount = 5000;
     private int fee;
 
     public Member(int adult, int teen, int child) {
